503b5aac2b1e4239f52268a561dd9fc5c9a47923
Alexander Goussas <goussasalexander@gmail.com>
2023-06-05 15:55:25 -0500

    Add concurrent queue for sharing updates

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 
 #include <httplib.h>
 #include <nlohmann/json.hpp>
+#include <blockingconcurrentqueue.h>
 
 #include "get_updates_payload.hpp"
 #include "get_updates_response.hpp"
@@ -11,25 +12,43 @@
 static char *BOT_TOKEN = nullptr;
 static std::string API_PREFIX;
 
-auto getUpdates(httplib::SSLClient &client) {
-    get_updates_payload payload;
-    payload.allowed_updates().push_back("messages");
+auto getUpdates(httplib::SSLClient &client, moodycamel::BlockingConcurrentQueue<get_updates_response> &queue) {
+    auto lastUpdateId = -1;
 
-    auto result = client.Post(
-            API_PREFIX + "/getUpdates",
-            nlohmann::json(payload).dump(),
-            "application/json"
-    );
+    for (;;) {
+        get_updates_payload payload;
 
-    if (!result) {
-        std::cerr << "Request for updates failed with error: " << result.error() << "\n";
-        return;
+        payload.timeout() = 60;
+        payload.offset() = lastUpdateId + 1;
+        payload.allowed_updates().emplace_back("messages");
+
+        auto result = client.Post(
+                API_PREFIX + "/getUpdates",
+                nlohmann::json(payload).dump(),
+                "application/json"
+        );
+
+        if (!result) {
+            std::cerr << "Request for updates failed with error: " << result.error() << "\n";
+            return;
+        }
+
+        nlohmann::json body = nlohmann::json::parse(result.value().body);
+        auto updates = body.at("result").get<std::vector<get_updates_response>>();
+
+        for (const auto &update: updates) {
+            if (update.update_id > lastUpdateId) {
+                lastUpdateId = update.update_id;
+            }
+            queue.enqueue(update);
+        }
     }
+}
 
-    nlohmann::json body = nlohmann::json::parse(result.value().body);
-    auto updates = body.at("result").get<std::vector<get_updates_response>>();
-
-    for (const auto &update: updates) {
+auto logUpdates(moodycamel::BlockingConcurrentQueue<get_updates_response> &queue) {
+    for (;;) {
+        get_updates_response update;
+        queue.wait_dequeue(update);
         std::cout << update.message.text << "\n";
     }
 }
@@ -46,7 +65,18 @@ auto main() -> int {
 
     httplib::SSLClient client("api.telegram.org");
 
-    getUpdates(client);
+    moodycamel::BlockingConcurrentQueue<get_updates_response> queue;
+
+    std::thread updatesThread([&]() {
+        getUpdates(client, queue);
+    });
+
+    std::thread loggingThread([&]() {
+        logUpdates(queue);
+    });
+
+    updatesThread.join();
+    loggingThread.join();
 
     return 0;
 }
